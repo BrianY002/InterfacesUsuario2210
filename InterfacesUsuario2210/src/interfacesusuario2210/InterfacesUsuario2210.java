fcc1a879cd5c2af04e3c8b53458afd1c655079ac
BrianY002 <104378087+BrianY002@users.noreply.github.com>
2022-05-06 21:19:21 -0500

    Add files via upload

---
+++ InterfacesUsuario2210/src/interfacesusuario2210/InterfacesUsuario2210.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfacesusuario2210;
+
+import ico.fes.iu.MiVentana;
+
+/**
+ *
+ * @author Azul6
+ */
+public class InterfacesUsuario2210 {
+
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MiVentana w=new MiVentana ();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
